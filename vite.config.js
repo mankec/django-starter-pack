cffadfa2af4638831232c3e76d42d5336f47e1e2
Milan Kecman <milan.kecman270@gmail.com>
2026-02-04 11:59:45 +0100

    chore: install TailwindCSS

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  root: '.',
+  build: {
+    outDir: "./core/static/dist",
+    emptyOutDir: true,
+    manifest: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, './core/static/src/js/main.js'),
+      }
+    }
+  },
+  plugins: [
+    tailwindcss(),
+  ],
+})
